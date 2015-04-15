d31c632a0e47043cad35a7e8bec6cc8d52dbf596
hecrp <alu0100774429@ull.edu.es>
2015-04-15 18:27:16 +0100

    app.js cambiado

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
+express = require('express')
+path = require('path')
+favicon = require('serve-favicon')
+logger = require('morgan')
+cookieParser = require('cookie-parser')
+bodyParser = require('body-parser')
+routes = require('./routes/index.coffee')
+app = express()
+# view engine setup
+app.set 'views', path.join(__dirname, 'views')
+app.set 'view engine', 'jade'
+app.use logger('dev')
+app.use bodyParser.json()
+app.use bodyParser.urlencoded(extended: false)
+app.use cookieParser()
+app.use express.static(path.join(__dirname, 'public'))
 
-/**
- * Module dependencies.
- */
+app.get '/', routes.index
 
-var express = require('express')
-  , routes = require('./routes');
+# catch 404 and forward to error handler
 
-var app = module.exports = express.createServer();
+app.use (req, res, next) ->
+  err = new Error('Not Found')
+  err.status = 404
+  next err
+  return
+# error handlers
+# development error handler
+# will print stacktrace
 
-// Configuration
+if app.get('env') == 'development'
+  app.use (err, req, res, next) ->
+    res.status err.status or 500
+    res.render 'error',
+      message: err.message
+      error: err
+    return
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+# production error handler
+# no stacktraces leaked to user
+app.use (err, req, res, next) ->
+  res.status err.status or 500
+  res.render 'error',
+    message: err.message
+    error: {}
+  return
+module.exports = app
